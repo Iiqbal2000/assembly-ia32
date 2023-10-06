77e7791a5119b6ecc80bbbbaea24980a22db433e
Iiqbal2000 <iqbalhafizh2000@gmail.com>
2023-10-06 19:33:39 +0000

    add: number to ascii converter

--- number_to_ascii_multiple_digit.s
+++ number_to_ascii_multiple_digit.s
@@ -1,32 +1,32 @@
 .section .data
-    result_str: .space 10  # Reservasi buffer untuk hasil string
+    result_str: .space 10  # Reservasi buffer untuk menyimpan hasil string
 
 .section .text
 .global _start
 
 _start:
-    movl $123, %eax  # Angka untuk dikonversi
-    leal result_str + 9, %edi  # Pointer ke akhir buffer
-    mov $'\n', (%edi)  # Menambahkan karakter baris baru di akhir
+    movl $123, %eax  # Angka yg akan dikonversi
+    leal result_str + 9, %edi  # Pointer ke akhir buffer untuk meletakkan '\n'
+    mov $'\n', (%edi)  # Menambahkan karakter '\n'
 
-    movl $10, %ebx  # Divisor
+    movl $10, %ebx  # 10 adalah pembagi untuk merubah number ke ascii
 
 convert_loop:
-    xorl %edx, %edx  # Membersihkan EDX untuk sisa pembagian
+    xorl %edx, %edx  # Reset EDX
     divl %ebx  # EAX = EAX / EBX, EDX = EAX mod EBX
     addl $'0', %edx  # Konversi sisa ke ASCII
     dec %edi  # Menggeser pointer buffer ke belakang
-    inc %ecx
+    inc %ecx  # ecx untuk menghitung jumlah karakter
     movb %dl, (%edi)  # Menyimpan digit ke buffer
     test %eax, %eax  # Mengecek apakah masih ada digit yang tersisa
     jnz convert_loop  # Jika ya, lanjutkan loop
 
     # Menulis string ke stdout
     movl $4, %eax  # syscall number for sys_write
-    movl $1, %ebx  # file descriptor 1 is stdout
+    movl $1, %ebx  # file descriptor untuk stdout
 
-    leal 1(%ecx), %edx
-    movl %edi, %ecx
+    leal 1(%ecx), %edx  # salin panjang karakter yg akan ditulis
+    movl %edi, %ecx # salin alamat buffer ke ecx agar bisa ditulis
 
     int $0x80  # Melakukan syscall
 
